1351ac0d6db7a6c4630b2d48e2d1cadd98d813e8
prashant <prashant.chaurasia@zehntech.com>
2022-09-27 11:56:56 +0530

    about rails & crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
 
   # root 'application#hello'
   root 'pages#home'
+  get 'about',to: 'pages#about'
 end
